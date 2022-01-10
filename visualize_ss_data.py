48c46abb95be8cf3a974520d76b99c39229cb5e1
gmberton <bertongabri@gmail.com>
2022-01-10 13:26:01 +0100

    Updated example of generated images

--- visualize_ss_data.py
+++ visualize_ss_data.py
@@ -21,7 +21,7 @@ def tensor_to_numpy(tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225
 
 
 def draw_quadrilateral(image, points=None, color=(0, 255, 0), thickness=20):
-    """Draw on image the quadrilateral defined by points"""
+    """Draw on image the quadrilateral defined by points."""
     if points is None:  # If points is None, draw on the edges of the image.
         points = torch.tensor([[-1, -1], [ 1, -1], [ 1,  1], [-1,  1]])
         points = points.type(torch.float)
@@ -39,30 +39,38 @@ def draw_quadrilateral(image, points=None, color=(0, 255, 0), thickness=20):
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--image_path", type=str, default="data/sample.jpg",
+parser.add_argument("--image_path", type=str, default="data/example.jpg",
                     help="path of image")
-parser.add_argument("--k", type=int, default=0.6,
+parser.add_argument("--k", type=int, default=0.8,
                     help="parameter k, defining the difficulty of ss training data")
 args = parser.parse_args()
 
-source_img = datasets_util.open_image_and_apply_transform(args.image_path)
-images, _, points = dataset_warp.get_random_homographic_pair(source_img, args.k, is_debugging=True)
+img_source_img = datasets_util.open_image_and_apply_transform(args.image_path)
+images, _, points = dataset_warp.get_random_homographic_pair(img_source_img, args.k, is_debugging=True)
 
 images = [tensor_to_numpy(i) for i in images]
-(t_x, t_y, _, _, t_a, t_b) = points
+# The two proj_intersections on the img_source image are equal
+(t_x, t_y, inters, inters, t_a, t_b) = points
 
-(source, img_a, img_b, intersection, _) = images
+(img_source, proj_a, proj_b, proj_intersection, _) = images
 
-source = draw_quadrilateral(source, t_x, color=(255,174,133))
-source = draw_quadrilateral(source, t_y, color=(154,179,255))
-img_a = draw_quadrilateral(img_a, t_a)
-img_a = draw_quadrilateral(img_a, color=(255,174,133))
-img_b = draw_quadrilateral(img_b, t_b)
-img_b = draw_quadrilateral(img_b, color=(154,179,255))
-intersection = draw_quadrilateral(intersection)
+# On each image, draw the colored quadrilateral
+img_source = draw_quadrilateral(img_source, t_x, color=(255,174,133))
+img_source = draw_quadrilateral(img_source, t_y, color=(154,179,255))
+img_source = draw_quadrilateral(img_source, inters)
+proj_a = draw_quadrilateral(proj_a, t_a)
+proj_a = draw_quadrilateral(proj_a, color=(255,174,133))
+proj_b = draw_quadrilateral(proj_b, t_b)
+proj_b = draw_quadrilateral(proj_b, color=(154,179,255))
+proj_intersection = draw_quadrilateral(proj_intersection)
 
-io.imsave("data/ss_source.jpg", source)
-io.imsave("data/ss_img_a.jpg", img_a)
-io.imsave("data/ss_img_b.jpg", img_b)
-io.imsave("data/ss_intersection.jpg", intersection)
+img_source = img_source.astype(np.uint8)
+proj_a = proj_a.astype(np.uint8)
+proj_b = proj_b.astype(np.uint8)
+proj_intersection = proj_intersection.astype(np.uint8)
+
+io.imsave("data/ss_img_source.jpg", img_source)
+io.imsave("data/ss_proj_a.jpg", proj_a)
+io.imsave("data/ss_proj_b.jpg", proj_b)
+io.imsave("data/ss_proj_inters.jpg", proj_intersection)
 
